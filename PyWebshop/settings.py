a0d767876e48270f2aa2b9ad23cb41f07f687881
Mario Mlinarić <mario@mariomlinaric.xyz>
2024-12-10 19:43:34 +0100

    first template and configuration

--- PyWebshop/settings.py
+++ PyWebshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'staticurls',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'PyWebshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +116,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = 'staticfiles/'
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Media files
+
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Receiving emails
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ staticurls/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StaticurlsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'staticurls'
